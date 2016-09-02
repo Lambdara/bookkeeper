5ff587d6bb9080386acbdba5bc127a007b03f570
Uwila <uwila@runbox.com>
2016-09-02 19:33:21 +0200

    Add printing to html

--- bookkeeper.py
+++ bookkeeper.py
@@ -1,3 +1,6 @@
+# Set a name
+name = "Bookkeeping"
+
 # Data is kept as a dict of `name:credit`
 data = dict()
 
@@ -26,3 +29,35 @@ def pay(payer,payees,subject,amount):
 
     record = (payer,payees,subject,amount)
     log.append(record)
+
+# Print current state to HTML file
+def make_html(path):
+    html = str()
+
+    html += "<!doctype html>\n"
+    html += "<html>\n"
+    html += "<h1>" + name + "</h1>\n"
+    html += "<h2>Balance</h2>\n"
+    html += "<table>\n"
+    html += "<tr><th>Name</th><th>Status</th><th>Amount</th></tr>\n"
+    for person in data.keys():
+        html += "<tr><td>" + person + "</td><td>"
+        if data[person] > 0:
+            html += "credit"
+        else:
+            html += "debt"
+        html += "</td><td>" + str(abs(data[person])) + "</td></tr>\n"
+    html += "</table>\n"
+    html += "<h2>Log</h2>\n"
+    html += "<table>\n"
+    html += "<tr><th>Payer</th><th>Payees</th><th>Subject</th><th>amount</th></tr>\n"
+    for (payer,payees,subject,amount) in log:
+        html += "<tr><td>" + payer + "</td>"
+        html += "<td>" + " ".join(payees) + "</td>"
+        html += "<td>" + subject + "</td>"
+        html += "<td>" + str(amount) + "</td></tr>\n"
+    html += "</table>\n"
+    html += "</html>\n"
+
+    with open(path,'w') as file:
+        file.write(html)
